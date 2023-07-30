aca37d4101a8a7d2950cb2b37a8c0e641f553bb9
葛海洲 <2019141410204@stu.scu.edu.cn>
2023-07-30 18:43:26 +0800

    update

--- src/urdfs/airbot_play_v1/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/ghz/ARX5/ros/src/arx5/arx5_urdfs/airbot_play_v1/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
