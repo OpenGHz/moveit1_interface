9263d96416ab7614c4069a09ce849ee3dc97ee13
葛海洲 <2019141410204@stu.scu.edu.cn>
2023-10-08 17:08:24 +0800

    feat(*):add setup tools and some models & worlds

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# set the color variables of 'echo' cmd
-DEF='\e[0m'
-OK='\e[1;32m'
-FAILED='\e[1;31m'
-
-# get the shell type
-str="${SHELL}" ; shtype=${str##*/};
-
 # export ros ws path
-source ./devel/setup."${shtype}" || { echo -e "${FAILED}Please use 'source' cmd to run this setup.sh file!${DEF}" && exit 0; }
+source ./devel/setup."${SHELL##*/}" || { echo -e "\e[1;31mPlease use 'source' cmd to run this setup.sh file!\e[0m" && exit 0; }
 
 # OK
-echo -e "${OK}OK!${DEF}"
+echo -e "\e[1;32mOK!\e[0m"
