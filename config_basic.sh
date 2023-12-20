3106aad76ba7a49ce1cf6e2ce7c5122a05f0f80a
葛海洲 <2019141410204@stu.scu.edu.cn>
2023-12-20 10:48:20 +0800

    update

--- config_basic.sh
+++ config_basic.sh
@@ -6,14 +6,15 @@ OK='\e[1;32m'
 WARN='\e[1;33m'
 
 # install useful robot tools such as interpolation, coordinate transformation, etc.
-git clone --depth 1 https://github.com/OpenGHz/robot_tools.git
-pip install ./robot_tools && rm -rf robot_tools
+echo "Trying to install packages from github..."
+git clone --depth 1 https://github.com/OpenGHz/robot_tools.git || exit 0
+pip install ./robot_tools && rm -rf robot_tools || exit 0
 # install inner python packages
-pip install ./src/airbot_play_follow_basic
+pip install ./src/airbot_play_follow_basic || exit 0
 
 # install the required ROS packages through rosdep
 rosdep install --from-path src --ignore-src -r -y || {
-	echo -e "${WARN}rosdep install failed (not complete), please check your rosdep.${DEF}"
+	echo -e "${WARN}rosdep install failed or not completely, please check your rosdep.${DEF}"
 	rosdepwarn=1
 }
 
