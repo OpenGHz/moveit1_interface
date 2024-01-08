8e5e4d6daf1858121cd2de4127173616d7900dc2
葛海洲 <2019141410204@stu.scu.edu.cn>
2024-01-08 11:16:48 +0800

    fix require bug

--- update.sh
+++ update.sh
@@ -13,7 +13,12 @@ else
 fi
 
 # $1 can be -e
-pip install "$1" ./robot_tools -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install "$1" ./src/airbot_play_follow_basic
+if [ -n "$1" ];then
+    pip install "$1" ./robot_tools -i https://pypi.tuna.tsinghua.edu.cn/simple
+    pip install "$1" ./src/airbot_play_follow_basic
+else
+    pip install ./robot_tools -i https://pypi.tuna.tsinghua.edu.cn/simple
+    pip install ./src/airbot_play_follow_basic
+fi
 
 echo -e "${OK}Update OK.${DEF}"
