a9f4aca4884c65fcf03e442808998284edea1aa0
OpenGHz <1352674740@qq.com>
2024-03-24 13:47:49 +0800

    pip install robotics_tools

--- config_basic.sh
+++ config_basic.sh
@@ -3,11 +3,7 @@ set -e
 # install tkinter
 sudo apt-get install python3-tk -y
 # install useful robot tools such as interpolation, coordinate transformation, etc.
-echo "Trying to install packages from github..."
-if [ ! -d "robot_tools" ]; then
-    git clone --depth 1 https://github.com/OpenGHz/robot_tools.git
-fi
-pip install ./robot_tools -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install robotics_tools
 # install inner python packages
 pip install ./src/airbot_play_ik_service
 pip install ./src/airbot_play_follow_basic -i https://pypi.tuna.tsinghua.edu.cn/simple
