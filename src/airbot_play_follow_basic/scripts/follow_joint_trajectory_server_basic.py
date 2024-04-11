4e6d6b342ae3239c7c0e1ae32876f0187205bb88
OpenGHz <1352674740@qq.com>
2024-04-11 13:11:13 +0800

    cmd now with names

--- src/airbot_play_follow_basic/scripts/follow_joint_trajectory_server_basic.py
+++ src/airbot_play_follow_basic/scripts/follow_joint_trajectory_server_basic.py
@@ -30,7 +30,7 @@ class MoveItAction(object):
         """
         self.interpolation = interpolation
         self.t_delta = 0.005  # 单位:s
-        self.init_pose = default_position  # 初始状态（单位为rad，防止零位干涉）
+        self.init_pose = default_position  # 初始状态（单位为rad，注意防止零位干涉）
         self.joint_nums = len(default_position)
 
         self.new_action = True  # do not send init cmd
@@ -40,12 +40,12 @@ class MoveItAction(object):
         self.cmd_publisher = rospy.Publisher(topic, JointState, queue_size=10)
         self.cmd = JointState()
         self.cmd.header.stamp = rospy.Time.now()
-        self.cmd.header.frame_id = "airbot_play"
+        self.cmd.header.frame_id = "airbot_play_moveit1"
         self.cmd.position = self.init_pose
-        self.cmd.velocity = [
-            0.8 for _ in range(self.joint_nums)
-        ]  # 初始化速度值23度/s，从而控制电机缓慢移动到0位
+        # 初始化速度值23度/s，从而控制电机缓慢移动到0位
+        self.cmd.velocity = [0.8 for _ in range(self.joint_nums)]
         self.cmd.effort = [0 for _ in range(self.joint_nums)]
+        self.is_sending_cmd = False
 
         rospy.Timer(rospy.Duration(1.0 / 200.0), self.control_continue)
 
@@ -57,44 +57,38 @@ class MoveItAction(object):
         )
         self._action_server.start()
 
-    def execute_cb(self, goal_handle: Union[FollowJointTrajectoryGoal, list]):
+    def execute_cb(self, goal_handle: FollowJointTrajectoryGoal):
         """Action Callback"""
         # 避免函数被重复运行
-        if not hasattr(self.execute_cb, "running"):
-            self.execute_cb.__dict__["running"] = False
-        if self.execute_cb.__dict__["running"]:
+        if self.is_sending_cmd:
             return
         else:
-            self.execute_cb.__dict__["running"] = True
+            self.is_sending_cmd = True
 
-        # 不同类型处理（获得统一的points变量）
-        points: List[JointTrajectoryPoint] = (
-            goal_handle.trajectory.points
-        )  # 获得目标途径上的所有轨迹点（无任何外在约束的情况下一般只有两个点）
-
+        # 获得目标途径上的所有轨迹点（无任何外在约束的情况下一般只有两个点）
+        points: List[JointTrajectoryPoint] = goal_handle.trajectory.points
+        joint_names = goal_handle.trajectory.joint_names
+        assert len(joint_names) == self.joint_nums
+        self.joint_names = joint_names
         # 构造初始矩阵
         times = len(points)
         time_array = np.zeros(times)  # 关节位置的时间信息列表
-        joints_matrix = np.zeros(
-            (times, self.joint_nums)
-        )  # 存储关节位置插值数据用的列表
+        # 存储关节位置插值数据用的列表
+        joints_matrix = np.zeros((times, self.joint_nums))
         joints_vel_matrix = joints_matrix.copy()
         joints_acc_matrix = joints_matrix.copy()
         # 分别获得各个点的时间信息和位置信息，放到两个列表中
         for time, point in enumerate(points):  # 单位为rad；每个point对应了某个时间点
-            time_array[time] = (
-                point.time_from_start.to_sec()
-            )  # 不同关节具有相同的时间点
-            joints_matrix[time] = (
-                point.positions
-            )  # point.positions[j]存储了关节j在当前时间的角度值；joints_matrix每一行存储了某个时间点所有关节的角度目标
+            # 不同关节具有相同的时间点
+            time_array[time] = point.time_from_start.to_sec()
+            # point.positions[j]存储了关节j在当前时间的角度值；joints_matrix每一行存储了某个时间点所有关节的角度目标
+            joints_matrix[time] = point.positions
             joints_vel_matrix[time] = point.velocities
             joints_acc_matrix[time] = point.accelerations
-
         # 轨迹插值
         execute_time_array = Interpolate.time_clip(
             0, time_array[times - 1], self.t_delta
-        )  # 将轨迹首尾点的时间以5ms来分段，与rospy.Rate(200)对应上
+        )  # 将轨迹首尾点的时间以t_delta来分段，与rospy.Rate(1/t_delta)对应上
         new_times = len(execute_time_array)
         if isinstance(self.interpolation, int):
             if self.interpolation == 1:  # 线性插值
@@ -113,16 +107,14 @@ class MoveItAction(object):
                     k=self.interpolation,
                 )
             else:
-                exit("暂不支持该类型的插值方式")
+                raise NotImplementedError(f"暂不支持{self.interpolation}插值")
         else:
-            exit("暂不支持该类型的插值方式")
-
+            raise NotImplementedError(f"暂不支持{self.interpolation}插值")
+        # 速度限制
         min_vel = 2.593
         max_vel = 4 * math.pi
-
-        joints_matrix_new = np.apply_along_axis(
-            interpolate, axis=0, arr=joints_matrix
-        )  # axis=0表示对各个列施加函数
+        # axis=0表示对各个列施加函数
+        joints_matrix_new = np.apply_along_axis(interpolate, axis=0, arr=joints_matrix)
         speed_matrix = np.apply_along_axis(
             Interpolate.linear_speed_calculate,
             axis=0,
@@ -144,21 +136,19 @@ class MoveItAction(object):
         self.new_action = True
         while self.continue_ok:
             pass
-        r = rospy.Rate(200)  # 200Hz，5ms控制频率
-        self.target_mode = rospy.get_param(
-            "/airbot_play/target_mode", default=-1
-        )  # 控制的对象确定（是否fake）
+        r = rospy.Rate(1 / self.t_delta)  # 控制频率设置
         for i in range(new_times):  # 遍历每一个插值点
             # 若有新的action中断发生，则立刻终止当前处理，转而下一个处理
             if self._action_server.is_preempt_requested():
                 self._action_server.set_preempted()
-                self.execute_cb.__dict__["running"] = False
+                self.is_sending_cmd = False
                 self.new_action = False
                 return
-            # 控制命令发布（只发6个关节的，即cmd长度为6）
+            # 控制命令发布
             self.cmd.velocity = speed_list[i]
             self.cmd.position = joints_list[i]
             self.cmd.header.stamp = rospy.Time.now()
+            self.cmd.name = self.joint_names
             self.cmd_publisher.publish(self.cmd)
             r.sleep()
         self.new_action = False
@@ -166,7 +156,7 @@ class MoveItAction(object):
         # 设置结果通知MoveIt执行完毕
         self._result.error_code = 0
         self._action_server.set_succeeded(self._result)
-        self.execute_cb.__dict__["running"] = False
+        self.is_sending_cmd = False
 
     def control_continue(self, event):
         """接收到控制消息后，发送完仍继续发送一段时间最后的目标值，防止丢包造成位置误差增大"""
