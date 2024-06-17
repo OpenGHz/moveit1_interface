a9369e0436df9c87625b6409f53a852ae56cdb44
Haizhou Ge <1352674740@qq.com>
2024-06-17 18:02:39 +0800

    fix airbot_ik pip install error

--- config_basic.sh
+++ config_basic.sh
@@ -5,7 +5,7 @@ sudo apt-get install python3-tk -y
 # install useful robot tools such as interpolation, coordinate transformation, etc.
 pip install robotics_tools
 # install inner python packages
-pip install ./src/airbot_play_ik_service
+pip install ./src/
 pip install ./src/airbot_play_follow_basic -i https://pypi.tuna.tsinghua.edu.cn/simple
 # OK
 echo -e "\033[32m[INFO] Install successfully! \033[0m"
