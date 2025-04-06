e8b79e3f676ad6fd3a0d43226940eae7f8912e5e
Haizhou Ge <1352674740@qq.com>
2025-04-06 20:54:08 +0800

    fix get eef pos

--- src/airbot_play_follow_basic/scripts/airbot_play_ros_interface.py
+++ src/airbot_play_follow_basic/scripts/airbot_play_ros_interface.py
@@ -63,7 +63,7 @@ class AirbotPlayRosInterface:
 
     def pub_joint_states(self, event):
         qpos = self.robot.get_joint_pos()
-        epos = (self.eef_factor - self.robot.get_eef_pos()) / 2
+        epos = (self.eef_factor - self.robot.get_eef_pos()[0]) / 2
         evel = self.robot.get_eef_vel()[0]
         eeef = self.robot.get_eef_eff()[0]
         self.joint_state.position = qpos + [epos, -epos]
